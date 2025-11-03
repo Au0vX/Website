7811b87bc3a48dd69316ce7161a3d7384464af0d
v8ri <ali.kashif3989@gmail.com>
2025-11-03 18:42:28 -0500

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Secret Page</title>
-  <link rel="stylesheet" href="style.css">
-  <style>
-    body { display: none; } /* hide page until password is correct */
-  </style>
-</head>
-<body>
-  <h1>Secret Page</h1>
-  <p>If you see this, you entered the correct password!</p>
 
-  <script src="secret.js"></script> <!-- Use separate script -->
-</body>
-</html>
